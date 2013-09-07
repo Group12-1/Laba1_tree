44befcec68ea2b64bf1864a0c0cb71e69c0604b4
Group12-1 <jerseyminsk@gmail.com>
2013-09-07 16:04:15 +0300

    first commit

---
+++ Apple.java
@@ -0,0 +1,14 @@
+package com.group12_1.tree;
+
+public class Apple {
+	int color;
+	boolean ripe;
+	int numStone;
+	
+	public Apple(int color, int numStone, boolean ripe){
+		this.color = color;
+		this.ripe = ripe;
+		this.numStone = numStone;
+	}
+	
+}
---
+++ Main.java
@@ -0,0 +1,33 @@
+package com.group12_1.tree;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) {
+		int numTree = 5; //колличчество деревьев
+		int numApple; // колличество €блок а дереве
+		Scanner sc = new Scanner(System.in); 
+		System.out.println("¬ведите колличество деревьев");
+		numTree = sc.nextInt();
+		Tree tr; 
+		List<Tree> tree = new ArrayList<Tree>();
+		
+		for(int i = 0; i < numTree; i++){
+			tree.add(tr = new Tree());
+		}
+		
+		for(int i = 0; i < tree.size(); i++){
+			numApple = tree.get(i).grow();
+			System.out.print(numApple + " ");
+		}
+	}
+
+}
