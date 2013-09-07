b70c8c5dcb0c319f95fedcd659e898982b50706b
Group12-1 <jerseyminsk@gmail.com>
2013-09-07 18:10:50 +0300

    Tree add method drop()

--- Tree.java
+++ Tree.java
@@ -2,10 +2,12 @@ package com.group12_1.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Random;
 
 public class Tree{
 	int numApple;
+	int drop;
 	Apple apple;
 	List<Apple> apl = new ArrayList<Apple>();
 	Random rnd = new Random();
@@ -17,6 +19,15 @@ public class Tree{
 	public int grow(){
 		for(int i = 0; i < numApple; i++)
 			apl.add(apple = new Apple(rnd.nextInt(10), rnd.nextBoolean()));
+		return apl.size();
+	}
+	
+	public int dropApple(){
+		drop = rnd.nextInt(apl.size());
+		int n = apl.size()-drop;
+		for (int i = apl.size(); i > n; i--){
+				apl.remove(i-1);
+		}
 		return apl.size();
 	}
 }
